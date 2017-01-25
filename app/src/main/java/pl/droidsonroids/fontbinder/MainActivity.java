183f921d2efc5ae0ae618d007935d4e5163ddfd4
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-01-25 01:29:58 +0100

    initial code added

---
+++ app/src/main/java/pl/droidsonroids/fontbinder/MainActivity.java
@@ -0,0 +1,22 @@
+package pl.droidsonroids.fontbinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+import com.example.BindFont;
+
+public class MainActivity extends Activity {
+	@BindFont("a")
+	TextView textA;
+
+	@BindFont("b4")
+	TextView textB;
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		MainActivityBinder.bind(this);
+	}
+}
