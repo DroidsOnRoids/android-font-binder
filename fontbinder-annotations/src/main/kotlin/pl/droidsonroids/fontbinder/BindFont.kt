9a020b136897d20b6697bab791fddec5ec791a4f
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-01-29 01:20:49 +0100

    default bold value changed to false

--- fontbinder-annotations/src/main/kotlin/pl/droidsonroids/fontbinder/BindFont.kt
+++ fontbinder-annotations/src/main/kotlin/pl/droidsonroids/fontbinder/BindFont.kt
@@ -2,4 +2,4 @@ package pl.droidsonroids.fontbinder
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FIELD)
-annotation class BindFont (val value: String, val bold:Boolean = true)
+annotation class BindFont(val value: String, val bold: Boolean = false)
